dbf64a69f6b64e63eb475cd4c0f948ce250ac3f6
Cameron Lares <cameronlares@gmail.com>
2020-10-05 23:03:04 -0400

    GET by ID completed, DELETE completed, ALL CRUD Completed

--- router/accounts-router.js
+++ router/accounts-router.js
@@ -18,8 +18,14 @@ router.get('/',(req, res)=>{
 
 router.get('/:id', (req, res) => {
     // select * from postman
-    const id = req.params.id
-    db('accounts').where('id', '=', id)
+    const {id} = req.params
+db.select('*').from("accounts").where({id}).first()// same as grabbing the first element from the array manuall with post[0]
+.then(account => {
+    res.status(200).json({data: account})
+})
+.catch(error => {
+    handleError(error, res)
+})
 });
 
 router.post('/', (req, res) => {
@@ -35,4 +41,40 @@ router.post('/', (req, res) => {
     })
 })
 
+router.put('/:id', (req, res) => {
+    const changes = req.body
+    const id = req.params.id
+    //please validate data before calling the database
+
+    db('accounts').where({ id: id }).update({changes}).then(count => {
+        res.status(201).json({data: count})
+    }) // another way of writing a where
+    .catch(error => {
+        res.status(500).json({ error: error.message})
+    })
+})
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    db("accounts")
+        .where({ id })
+        .del() // don't forget to have a where
+        .then(count => {
+            // count is the number of records deleted
+            if (count > 0) {
+                res.status(200).json({ data: count });
+            } else {
+                res.status(404).json({ message: "there was no record to delete" });
+            }
+        })
+        .catch(error => {
+            handleError(error, res);
+        });
+});
+
+function handleError(error, res) {
+    console.log("error", error);
+    res.status(500).json({ message: error.message });
+}
 module.exports = router;
